01ab2837bb71354bfb8cd719c4beb0909b2c193b
John MacLaughlin <>
2018-06-10 15:43:24 -0700

    Logging new event with tests (Not deleting old events yet)

--- counttracker/counttracker.py
+++ counttracker/counttracker.py
@@ -1,4 +1,6 @@
 from collections import deque
+import time
+
 from .event_second import EventSecond
 
 class CountTracker:
@@ -7,10 +9,66 @@ class CountTracker:
     Precision is to the current second.
     """
     def __init__(self):
+        # History is a double ended queue, acting as a doubly linked list.
+        # It will remain sorted by timestamp, with earlier times occurring further to the left,
+        #   and later times occurring further to the right
+        # Events that occur earlier than the MEMORY_TIME_LIMIT will be removed in history
         self._history = deque()
 
+        self._MEMORY_TIME_LIMIT = 300  # 5 minutes * 60 seconds per minute
+
+    def _current_time_is_in_history(self, current_time):
+        """
+
+        :param current_time: time in seconds
+        :return: True if the current time has already been logged, else False
+        """
+        try:
+            last_event = self._history[-1]
+
+            if last_event.timestamp == int(current_time):
+                return True
+            else:
+                return False
+        except IndexError:
+            # History is empty
+            return False
+
+    def _remove_old_events(self, current_time):
+        """
+        Remove old events from history
+        An old event is one that occurred more than MEMORY_TIME_LIMIT seconds before the current time
+        :param current_time: time in seconds
+        :return:
+        """
+        pass
+
     def log_event(self):
-        raise NotImplementedError("log_event not implemented yet.")
+        """
+        Log event at the current time (second).
+
+        If an event has already occurred at the current second, increase that second's count by 1.
+        Otherwise, append a new second with a count of 1.
+
+        Also deletes records of events that occurred before MEMORY_TIME_LIMIT before the current time
+
+        :return: None
+        """
+
+        # Log current event
+        current_time = time.time()
+
+        if self._current_time_is_in_history(current_time):
+            # Increase counter of last timestamp by 1
+            self._history[-1].log_event()
+        else:
+            # Must add new Event Second to history because this is a new second
+            new_second = EventSecond(current_time)
+            new_second.log_event()
+            self._history.append(new_second)
+
+        # Remove old events
+        self._remove_old_events(current_time)
 
     def get_event_counts(self, duration):
         raise NotImplementedError("log_event not implemented yet.")
--- tests/test_counttracker.py
+++ tests/test_counttracker.py
@@ -1,6 +1,77 @@
+import time
+
 from .context import counttracker
 
 
 def test_init():
-    ct = counttracker.CountTracker()
-    assert ct is not None
+    tracker = counttracker.CountTracker()
+    assert tracker is not None
+
+
+### Test _current_time_is_in_history ###
+def test_current_time_is_in_history_empty():
+    tracker = counttracker.CountTracker()
+    current_time = time.time()
+
+    assert not tracker._current_time_is_in_history(int(current_time))
+
+
+def test_current_time_is_in_history_false():
+    tracker = counttracker.CountTracker()
+    current_time = time.time()
+    tracker.log_event()
+
+    assert not tracker._current_time_is_in_history(int(current_time) + 1)
+
+
+def test_current_time_is_in_history_true():
+    tracker = counttracker.CountTracker()
+    current_time = time.time()
+    tracker.log_event()
+
+    assert tracker._current_time_is_in_history(int(current_time))
+
+
+### Test log event ###
+def test_log_event_one_event():
+    tracker = counttracker.CountTracker()
+
+    current_time = time.time()
+
+    tracker.log_event()
+    history = tracker._history
+
+    assert len(history) == 1
+    assert history[0].timestamp == int(current_time)
+    assert history[0].count == 1
+
+
+def test_log_event_previous_event_is_same_time():
+    tracker = counttracker.CountTracker()
+
+    current_time = time.time()
+
+    tracker.log_event()
+    tracker.log_event()
+    history = tracker._history
+
+    assert len(history) == 1  # Still only one event second is logged
+    assert history[0].timestamp == int(current_time)
+    assert history[0].count == 2 # Two events occurred at only event second
+
+
+def test_log_event_previous_event_is_different_time():
+    tracker = counttracker.CountTracker()
+
+    current_time = time.time()
+
+    previous_second = counttracker.EventSecond(current_time - 1)
+    previous_second.log_event()
+    tracker._history.append(previous_second)
+    tracker.log_event()
+    history = tracker._history
+
+    assert len(history) == 2  # Now two event seconds are logged
+    assert history[0].timestamp != int(current_time)
+    assert history[0].count == 1  # old event second has count 1
+    assert history[1].count == 1  # new event second has count 1
