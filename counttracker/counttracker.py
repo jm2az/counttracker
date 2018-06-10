73045fa39ba2ef221f66a09352722a87e20538a2
John MacLaughlin <>
2018-06-10 14:55:12 -0700

    Couple of docs on counttracker

--- counttracker/counttracker.py
+++ counttracker/counttracker.py
@@ -1,3 +1,16 @@
+from collections import deque
+from .event_second import EventSecond
+
 class CountTracker:
+    """
+    Keeps track of the counts of events that have happened over the past 5 minutes.
+    Precision is to the current second.
+    """
     def __init__(self):
-        pass
+        self._history = deque()
+
+    def log_event(self):
+        raise NotImplementedError("log_event not implemented yet.")
+
+    def get_event_counts(self, duration):
+        raise NotImplementedError("log_event not implemented yet.")
